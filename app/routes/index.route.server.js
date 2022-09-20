a94bc9a0deabb6d3849751e6792640ff8b605763
Heba Sajid <hsajid35467@gmail.com>
2022-09-19 22:12:18 -0400

    added different EJS and CSS and JS files

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { goodbyeWorld, helloWorld } from "../controllers/index.controller.server.js";
+import { displayHomePage } from "../controllers/index.controller.server.js";
 
 const router = Router();
 
-router.get('/hello', helloWorld);
-router.get('/goodbye', goodbyeWorld);
+router.get('/', displayHomePage);
 
 export default router;
