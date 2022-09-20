a91a59113b6797b9e5b54b6ec2bc9a7781096df5
Heba Sajid <hsajid35467@gmail.com>
2022-09-19 21:39:54 -0400

    updates

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/client')));
-app.use(sesstion({
+app.use(session({
     secret: 'MySecret',
     saveUninitialized: false,
     resave: false
